b8e53e1faa80ff4b5aecbbed1ec86abd112b75f7
Shanto57575 <shanto57575@gmail.com>
2024-06-05 17:33:46 +0600

    finally done

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,17 +1,8 @@
 const Download = () => {
 	return (
-		<div className="mb-48 md:mb-[600px]">
-			<div className="relative">
-				<img
-					className="hidden lg:block absolute md:-top-32 md:-left-60"
-					src="https://i.ibb.co/6tKxgMG/scat-Color.png"
-					alt=""
-				/>
-				<img
-					className="absolute md:top-0 md:left-0"
-					src="https://i.ibb.co/NgRTSq3/download.png"
-					alt=""
-				/>
+		<div>
+			<div>
+				<img src="https://i.ibb.co/NgRTSq3/download.png" alt="" />
 			</div>
 		</div>
 	);
